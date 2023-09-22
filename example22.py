010cbf4be55718944fa2dd5762dfc72192141959
scientlogistics <rohin@scientsolutions.ca>
2023-09-22 19:55:51 -0400

    editted exmaple naming scheme

--- example22.py
+++ example22.py
@@ -1,9 +1,10 @@
 from flask import Flask 
 app = Flask( __name__ ) 
+
 @app.route('/') 
 def index(): 
     return 'Hello World!' 
 
-@app.route('/user/')
+@app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
