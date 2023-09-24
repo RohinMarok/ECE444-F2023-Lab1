4581d4960630ddabe2d83d88cb4444a5e2c46805
scientlogistics <rohin@scientsolutions.ca>
2023-09-24 14:15:31 -0400

    finished modifying example 3

---
+++ example3.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template 
+from flask_moment import Moment
+from datetime import datetime
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__, template_folder='sheets')
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/') 
+def index(): 
+    return render_template('index.html') 
+
+@app.route('/user/<name>')
+def user(name): 
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
